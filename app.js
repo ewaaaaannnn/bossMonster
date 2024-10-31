54431bae7b6e594fb8989c3eac693d383ccbfb20
ewaaaaannnn <ewanferg01@gmail.com>
2024-10-31 15:55:19 -0600

    Fully Completed Goals

--- app.js
+++ app.js
@@ -17,17 +17,18 @@ const boss = {
   health: 100,
   maxHealth: 100,
   damage: 5,
-  level: 1
+  level: 1,
+  timesBeat: 0,
 }
 
 let heroDmg = 0
 
+let gold = 0
 
-
-
+let levelCost = 100
 //!SECTION LOGIC
 
-
+let potionCost = 25
 
 
 function addHeroAttack() {
@@ -46,7 +47,7 @@ function attackBoss() {
   heroDmg = 0
   resetBoss()
   drawBoss()
-
+  drawLevel()
 }
 
 function bossAttacks() {
@@ -63,15 +64,47 @@ function resetBoss() {
     boss.health += boss.maxHealth * 2;
     boss.maxHealth = boss.health
     boss.level++
-
+    boss.timesBeat++
+    gold += 25 * boss.timesBeat
   }
 }
 
+function buyPotion(heroName) {
+  for (let i = 0; i < heroes.length; i++) {
+    const hero = heroes[i];
+    if (hero.name == heroName) {
+      if (gold >= 25) {
+        hero.health += 50
+        gold -= 25
+        drawHeroStats()
+        drawLevel()
+      }
+      else {
+        window.alert(`You cannot afford the Potion!`)
+        // NOTE hard stop! Do not continue this function!
+        return
+      }
+    }
+  }
+}
 
-
-
-
-
+function levelUp(heroName) {
+  const foundHero = heroes.find(hero => hero.name == heroName)
+  if (gold >= levelCost) {
+    foundHero.damage *= 2
+    foundHero.health *= 2
+    gold -= 25
+    levelCost = levelCost * 2
+    drawHeroStats()
+    drawLevel()
+    drawLevelCost()
+  }
+  else {
+    window.alert(`You cannot afford the Level Up!`)
+    // NOTE hard stop! Do not continue this function!
+    return
+  }
+}
 
 
 
@@ -82,7 +115,7 @@ function resetBoss() {
 
 function drawBoss() {
   const bossElm = document.getElementById('boss');
-  bossElm.innerHTML = `<p>${boss.health} ${boss.maxHealth} ${boss.level}</p>`;
+  bossElm.innerHTML = `<p>${boss.health} ${boss.maxHealth} </p>`;
 }
 
 drawBoss()
@@ -93,14 +126,26 @@ function drawHeroStats() {
     const hero = heroes[i];
     const heroElem = document.getElementById(hero.name)
     if (heroElem) {
-      heroElem.innerHTML = `<p>${hero.name} ${hero.health}</p>`;
+      heroElem.innerHTML = `<p>${hero.name} ${hero.health} ${hero.damage}</p>`;
     }
   }
 }
 drawHeroStats()
 
 
+function drawLevel() {
+  const bossElm = document.getElementById('stats');
+  bossElm.innerHTML = `<p>${boss.timesBeat} ${boss.level} ${gold} </p>`;
+}
 
+drawLevel()
+
+function drawLevelCost() {
+  const bossElm = document.getElementById('prices');
+  bossElm.innerHTML = `<p>${levelCost} ${potionCost}</p>`;
+}
+
+drawLevelCost()
 
 //!SECTION DOCUMENT LOADING
 
